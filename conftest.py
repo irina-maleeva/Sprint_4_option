a54b14cd73cb744585161ec55ad02ca066fc2255
irina-maleeva <iwmaleeva@gmail.com>
2023-03-22 17:27:32 +0300

    changed browser to Chrome, as Firefox does not work with my Windows 11 -  OSError: [WinError 216] Эта версия "%1" не совместима с версией Windows, работающей на этом компьютере. Проверьте сведения о системе, а затем обратитесь к издателю программного обеспечения

--- conftest.py
+++ conftest.py
@@ -2,16 +2,8 @@ import pytest
 from selenium import webdriver
 
 @pytest.fixture()
-def driver1():
-    driver = webdriver.Chrome()
-    driver.get('https://qa-scooter.praktikum-services.ru/')
-    yield driver
-    driver.quit()
-
-
-@pytest.fixture()
 def driver():
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
     driver.get('https://qa-scooter.praktikum-services.ru/')
     yield driver
     driver.quit()
